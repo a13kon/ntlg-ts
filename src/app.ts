212c75a2e13d0244da1b58cc2a8509153275b604
a13con <a13kon@yandex.ru>
2024-01-19 16:00:29 +0300

    eslint

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,2 @@
-interface Book {
-    name: string;
-    isbn: string;
-}
-
-const book: Book = {
-    name: "MyBook",
-    isbn: "123123"
-};
-
-const container = document.getElementById('content');
-
-
-if (container) {
-    container.textContent = `book name is ${book.name}, ISBN ${book.isbn}`;
-}
+const myname: string = "Ivan";
+console.log(myname);

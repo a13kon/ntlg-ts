e1ee7cc8b6a6865c6113034cec7d3615754be512
a13con <a13kon@yandex.ru>
2024-01-26 17:05:07 +0300

    ts

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,29 @@
 "use strict";
-function main() {
-    var person = {
-        name: 'Ivan',
-        age: 34
-    };
-    person = {
-        name: 'Alex',
-        age: 24
-    };
-}
+var isTrue = true;
+var isNumber = 1;
+var list = ['10', '20', '30'];
+list.push('40');
+var lengt = list.map(function (item) {
+    item.length;
+});
+var max;
+max = [10, 20, '10'];
+max.push(20);
+max.push('qwe');
+console.log(max);
+var Item;
+(function (Item) {
+    Item[Item["FETCHING"] = 0] = "FETCHING";
+    Item[Item["SUCCESS"] = 1] = "SUCCESS";
+    Item[Item["FAILED"] = 2] = "FAILED";
+})(Item || (Item = {}));
 ;
+var Book = /** @class */ (function () {
+    function Book() {
+        this.hasHardCover = true;
+    }
+    return Book;
+}());
+var primaryColor = 'red';
+primaryColor = 0x8e8e8e;
+console.log(primaryColor);
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,42 @@
-function main() {
-    let person = {
-        name: 'Ivan',
-        age: 34
-    };
-    person = {
-        name: 'Alex',
-        age: 24
-    };
+let isTrue: boolean = true;
+let isNumber: number = 1; 
+let list: string[] = ['10', '20', '30'];
+
+list.push('40');
+let lengt = list.map((item) => {
+    item.length
+})
+
+let max: [number, number, string];
+max = [10, 20, '10'];
+max.push(20);
+max.push('qwe');
+
+console.log(max);
+
+enum Item {
+    FETCHING,
+    SUCCESS,
+    FAILED
 };
+
+interface PrintedItem {
+    name: string;
+    isbn: string;
+    pagesCounte: number;
+    hasHardCover: boolean;
+}
+
+class Book implements PrintedItem {
+    name: string;
+    isbn: string;
+    pagesCounte: number;
+    hasHardCover = true;
+}
+
+type ColorType = string | number;
+
+let primaryColor: ColorType = 'red';
+primaryColor = 0x8e8e8e;
+
+console.log(primaryColor);

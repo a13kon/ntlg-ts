91176827909bf358cfb3edbaef6d414a681f10e0
a13con <a13kon@yandex.ru>
2024-01-29 17:47:10 +0300

    ex4

--- dist/app.js
+++ dist/app.js
@@ -15,49 +15,40 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var User = /** @class */ (function () {
-    function User(name, email) {
+    function User(name, lastName) {
         this.name = name;
-        this.email = email;
+        this.lastName = lastName;
     }
+    User.prototype.setCart = function (cart) {
+        this.cart = cart;
+    };
     return User;
 }());
-var BaseTariff = /** @class */ (function () {
-    function BaseTariff(user) {
-        this.firstDay = true;
-        this.type = 'base';
-        this.user = user;
+var Cart = /** @class */ (function () {
+    function Cart() {
     }
-    BaseTariff.prototype.showUserName = function () {
-        console.log(user.name);
+    Cart.prototype.refresh = function () {
     };
-    return BaseTariff;
+    return Cart;
 }());
-var BaseTariffPlus = /** @class */ (function (_super) {
-    __extends(BaseTariffPlus, _super);
-    function BaseTariffPlus() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.secndDay = true;
-        _this.recording = true;
-        _this.type = 'base-plus';
-        return _this;
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    return BaseTariffPlus;
-}(BaseTariff));
-var PremiumTariff = /** @class */ (function (_super) {
-    __extends(PremiumTariff, _super);
-    function PremiumTariff(user, firstDay, dinner) {
-        var _this = _super.call(this, user) || this;
-        _this.networking = true;
-        _this.type = 'premium';
-        _this.firstDay = firstDay;
-        _this.dinner = dinner;
-        return _this;
+    Teacher.prototype.setScore = function (student) {
+        //
+    };
+    return Teacher;
+}(User));
+var Student = /** @class */ (function () {
+    function Student() {
     }
-    return PremiumTariff;
-}(BaseTariffPlus));
-var user = new User('myName', 'myEmail');
-// user.name = 'myName';
-// user.email = 'myEmail';
-var test = new PremiumTariff(user, false, true);
-test.showUserName();
-console.log(test);
+    Student.prototype.getTotalScore = function () {
+        //
+        return 0;
+    };
+    return Student;
+}());
+var user = new User('Oleg', 'Petrov');
+var userCart = new Cart();

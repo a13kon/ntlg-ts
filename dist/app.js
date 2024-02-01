7634f9bf8c6661ad0ac019b99bc0c887a2e3b0a0
a13kon <a13kon@yandex.ru>
2024-02-01 16:55:27 +0300

    ex6

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var User = /** @class */ (function () {
     function User() {
     }
@@ -11,3 +26,28 @@ var User = /** @class */ (function () {
 var user = new User;
 console.log(User.MAX_LEVEL);
 console.log(User.generatePassword());
+var Figure = /** @class */ (function () {
+    function Figure() {
+    }
+    Figure.prototype.getArea = function () {
+        console.log('Not Implemented');
+        return 0;
+    };
+    return Figure;
+}());
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle(width, height) {
+        var _this = _super.call(this) || this;
+        _this.width = width;
+        _this.height = height;
+        return _this;
+    }
+    Rectangle.prototype.getArea = function () {
+        console.log('Not Implemented');
+        return this.width * this.height;
+    };
+    return Rectangle;
+}(Figure));
+var figure = new Rectangle(20, 80);
+console.log(figure.getArea());
--- src/app.ts
+++ src/app.ts
@@ -1,106 +1,36 @@
-class HumanWorker {
-    name: string;
-
-    constructor(name: string) {
-        this.name = name;
-    }
-
-    doWork(): void {
-        console.log('working...');
+class User {
+    static MAX_LEVEL = 80;
+    static generatePassword(): string {
+        return Math.random().toString(36).substring(2);
     }
 }
 
-class Lumberjack extends HumanWorker{
-    doWork(): void {
-        console.log('cutting...');
+const user = new User;
+console.log(User.MAX_LEVEL);
+console.log(User.generatePassword());
+
+
+abstract class Figure {
+    getArea(): number {
+        console.log('Not Implemented');
+        return 0;
     }
 }
 
-class Hunter extends HumanWorker {
-    doWork(): void {
-        console.log('hunting...');
+
+class Rectangle extends Figure {
+    width: number;
+    height: number;
+    constructor(width: number, height: number){
+        super();
+        this.width = width;
+        this.height = height;
+
+    }
+    getArea(): number {
+        console.log('Not Implemented');
+        return this.width * this.height;
     }
 }
-
-const worker1 = new Hunter('human1');
-worker1.doWork();
-
-const worker2 = new Lumberjack('human2');
-worker2.doWork();
-
-///////////////////////////////////////////////////
-console.log();
-
-class Book {
-    name: string;
-    author: string;
-
-    constructor(name: string, author: string) {
-        this.name = name;
-        this.author = author;
-    }
-
-    getInfo():string {
-        return `${this.name} - ${this.author}`;
-    }
-}
-
-class ComicsBook extends Book {
-    type: string;
-
-    constructor(type: string, name: string, author: string ) {
-        super(name, author);
-        this.type = type;
-
-    }
-
-    getInfo(): string {
-        const bookInfo = super.getInfo();
-        return `${bookInfo} - ${this.type}`;
-    }
-}
-
-const book: Book = new Book('book', 'you');
-const comics: ComicsBook = new ComicsBook('comicsBook', 'comics', 'me');
-
-console.log(book.getInfo());
-console.log(comics.getInfo());
-
-///////////////////////////////////////////////////
-console.log();
-
-function sum(a: number): number;
-function sum(a: number, b: number): number;
-function sum(a: number, b: number, c: number): number;
-// function sum(a: number, b?: number, c?: number): number {
-//     let sum: number = a;
-//     if (b) {
-//         sum += b;
-//     }
-
-//     if (c) {
-//         sum += c;
-//     }
-
-//     return sum;
-// };
-
-function sum(a: number, b: number = 0, c: number = 0): number {
-    return a + b + c;
-};
-
-
-console.log(sum(10));
-console.log(sum(10, 15));
-console.log(sum(10, 15, 10));
-
-
-// function testSpread(...a: number[]): number {
-//     let sum = a.reduce((acc, elem) => 
-//         acc + elem
-//     )
-
-//     return sum;
-// }
-
-// console.log(testSpread(10,10,10,10,10,10));
+const figure = new Rectangle(20, 80);
+console.log(figure.getArea());

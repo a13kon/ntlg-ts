252891cfeee5c510eb239e7ad6ccc4b693f0ec76
a13kon <a13kon@yandex.ru>
2024-02-07 13:16:19 +0300

    contract

--- dist/app.js
+++ dist/app.js
@@ -1,68 +1,21 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var BooksRepository = /** @class */ (function () {
-    function BooksRepository() {
+var UserRepository = /** @class */ (function () {
+    function UserRepository() {
     }
-    BooksRepository.prototype.createBook = function (book) {
-        //create book
-        console.log(book);
+    UserRepository.prototype.findAll = function () {
+        // ...
+        console.log('all users');
     };
-    BooksRepository.prototype.getBook = function (id) {
-        //get book by id
-    };
-    BooksRepository.prototype.getBooks = function () {
-        //get all books
-    };
-    BooksRepository.prototype.updateBooks = function (id) {
-        //update book
-    };
-    BooksRepository.prototype.deleteBook = function (id) {
-        //delete book
-    };
-    return BooksRepository;
+    return UserRepository;
 }());
-var MainStorage = /** @class */ (function (_super) {
-    __extends(MainStorage, _super);
-    function MainStorage() {
-        return _super !== null && _super.apply(this, arguments) || this;
+var UserService = /** @class */ (function () {
+    function UserService() {
+        this.repository = new UserRepository();
     }
-    return MainStorage;
-}(BooksRepository));
-// class Magazines implements Book {
-//     title: string;
-//     description: string;
-//     authors: string;
-//     favorite: boolean;
-//     fileCover: string;
-//     fileName: string;
-//     constructor(title: string, 
-//                 description: string, 
-//                 authors: string, 
-//                 favorite: boolean,
-//                 fileCover: string,
-//                 fileName: string) {
-//                     this.title = title;
-//                     this.description = description;
-//                     this.authors = authors;
-//                     this.favorite = favorite;
-//                     this.fileCover = fileCover;
-//                     this.fileName = fileName;
-//                 }
-// }
-// const storage = new MainStorage();
-// const magazine = new Magazines('title', 'desc', 'authors', true, 'corev', 'name');
-// storage.createBook(magazine);
+    UserService.prototype.getAllUsers = function () {
+        return this.repository.findAll();
+    };
+    return UserService;
+}());
+var userServ = new UserService();
+userServ.getAllUsers();
--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,20 @@
-interface Book {
-
-    title: string;
-    description: string;
-    authors: string;
-    favorite: boolean;
-    fileCover: string;
-    fileName: string;
-}
-
-abstract class BooksRepository {
-
-    createBook(book: Book) {
-        //create book
-        console.log(book);
-    }
-
-    getBook(id: number) {
-        //get book by id
-    }
-
-    getBooks(){
-        //get all books
-    }
-
-    updateBooks(id: number) {
-        //update book
-    }
-
-    deleteBook(id: number) {
-        //delete book
+class UserRepository {
+    findAll() {
+        // ...
+        console.log('all users')
     }
 }
 
-class MainStorage extends BooksRepository {
-    
+class UserService {
+    repository: UserRepository;
+    constructor() {
+        this.repository = new UserRepository();
+    }
+
+    getAllUsers() {
+        return this.repository.findAll();
+    }
 }
 
-// class Magazines implements Book {
-
-//     title: string;
-//     description: string;
-//     authors: string;
-//     favorite: boolean;
-//     fileCover: string;
-//     fileName: string;
-
-//     constructor(title: string, 
-//                 description: string, 
-//                 authors: string, 
-//                 favorite: boolean,
-//                 fileCover: string,
-//                 fileName: string) {
-
-//                     this.title = title;
-//                     this.description = description;
-//                     this.authors = authors;
-//                     this.favorite = favorite;
-//                     this.fileCover = fileCover;
-//                     this.fileName = fileName;
-//                 }
-// }
-
-// const storage = new MainStorage();
-// const magazine = new Magazines('title', 'desc', 'authors', true, 'corev', 'name');
-// storage.createBook(magazine);
+const userServ = new UserService();
+userServ.getAllUsers();
